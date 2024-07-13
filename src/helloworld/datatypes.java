863b0ec3ab5ffcafa5ed0a7adbcb90c832d8eef9
Admin <Admin@DESKTOP-N574POQ>
2024-07-14 01:05:34 +0530

    initial commit

---
+++ src/helloworld/datatypes.java
@@ -0,0 +1,28 @@
+package datatypes;
+
+public class datatypes {
+
+	public static void main(String[] args) 
+	{
+		int value1 =3/2;
+		System.out.println("value1="+value1);
+		int side=2;
+		int area=side*side;
+		System.out.println("Area of square="+area);
+		char ch1=78;
+		System.out.println(ch1);
+		int a='N';
+		System.out.println(a);
+		
+		
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+		
+		float f =22.14f;
+		double d=f;
+		System.out.println(d);
+		
+		
+	}
+}
